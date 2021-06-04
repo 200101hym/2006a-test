b4279e78b55f235712983c83a6b44e84f5f0cb7e
HUAWEI <3304194001@qq.com>
2021-06-04 14:43:29 +0800

    初始化项目

--- src/main/java/com/example/h2006atest/TestController.java
+++ src/main/java/com/example/h2006atest/TestController.java
@@ -1,17 +1,9 @@
 package com.example.h2006atest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
-    @RequestMapping("test")
-    public String test(){
-        return "test------1";
-    }
-
-
 
 
 }
